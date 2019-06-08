c1e52a68a70e659bad21ed1111cca671912278ea
chiaojfang <chiaojfang@gmail.com>
2019-06-08 12:52:24 +0800

    updated master_device.c

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -7,3 +7,10 @@
 #include <linux/errno.h>
 #include <linux/net.h>
 #include <linux/fs.h>
+#include <linux/netdevice.h>
+#include <linux/ip.h>
+#include <linux/in.h>
+#include <linux/misdevice.h>
+#include <linux/delay.h>
+#include <linux/slabs.h>
+#include <linux/mm.h>
