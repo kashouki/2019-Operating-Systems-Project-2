02b60b4196c5ef5bf29c98cfa1b96e94a3522eae
chiaojfang <chiaojfang@gmail.com>
2019-06-09 22:21:56 +0800

    updated master_device

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -21,6 +21,8 @@
 #include <asm/pgtable.h>
 #include <asm/highmem.h>
 
+
+
 #define DEFAULT_PORT 2325
 #define master_IOCTL_CREATESOCK 0x12345677
 #define master_IOCTL_MMAP 0x12345678
@@ -28,6 +30,30 @@
 #define BUF_SIZE 512
 #define MAP_SIZE PAGE_SIZE * 100
 typedef struct socket * ksocket_t;
+struct debugentr *file1;
+
+
+
 
 //exported functions
 extern ksocket_t ksocket( int domain, int type, int protocol);
+extern int kbind(ksocket_t socket, struct sockaddr *address, int address_len);
+extern int klisten(ksocket_t socket, int backlog);
+extern ksocket_t kaccept(ksocket_t socket, int backlog);
+extern ssize_t ksend(ksocket_t socket, const void *buffer, size_t length, int flag);
+extern int kclose(ksocket_t socket);
+extern char *inet_ntoa(struct in_addr *in);
+
+static int __init master_init(void);
+static void __exit master_exit(void);
+
+int master_close(struct inode *inode, struct file *filp);
+int master_open(struct inode *inode, struct file *filp);
+static long master_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param);
+static ssize_t send_msg(struct file *file, const char __user *buf, size_t count, loff_t, *data);
+
+static ksocket_t sockfd_srv, sockfd_cli;
+static struct sockaddr_in addr_srv;
+static struct sockaddr_in addr_cli;
+static mm_segment_t old_fs;
+static int addr_len;
