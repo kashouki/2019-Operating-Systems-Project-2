41cff08c3628a5b1ee97e243713502366fcdde88
chiaojfang <chiaojfang@gmail.com>
2019-06-09 17:09:35 +0800

    updated master_device

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -14,3 +14,20 @@
 #include <linux/delay.h>
 #include <linux/slabs.h>
 #include <linux/mm.h>
+#include <net/sock.h>
+#include <asm/processor.h>
+#include <asm/uaccess.h>
+#include <asm/page.h>
+#include <asm/pgtable.h>
+#include <asm/highmem.h>
+
+#define DEFAULT_PORT 2325
+#define master_IOCTL_CREATESOCK 0x12345677
+#define master_IOCTL_MMAP 0x12345678
+#define master_IOCTL_EXIT 0x12345679
+#define BUF_SIZE 512
+#define MAP_SIZE PAGE_SIZE * 100
+typedef struct socket * ksocket_t;
+
+//exported functions
+extern ksocket_t ksocket( int domain, int type, int protocol);
