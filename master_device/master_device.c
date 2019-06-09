2133a3eb8010f514744d1a0cb45359bd93aae044
chiaojfang <chiaojfang@gmail.com>
2019-06-10 00:21:31 +0800

    added to master_device.c

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -10,9 +10,9 @@
 #include <linux/netdevice.h>
 #include <linux/ip.h>
 #include <linux/in.h>
-#include <linux/misdevice.h>
+#include <linux/miscdevice.h>
 #include <linux/delay.h>
-#include <linux/slabs.h>
+#include <linux/slab.h>
 #include <linux/mm.h>
 #include <net/sock.h>
 #include <asm/processor.h>
@@ -36,11 +36,11 @@ struct debugentr *file1;
 
 
 //exported functions
-extern ksocket_t ksocket( int domain, int type, int protocol);
+extern ksocket_t ksocket(int domain, int type, int protocol);
 extern int kbind(ksocket_t socket, struct sockaddr *address, int address_len);
 extern int klisten(ksocket_t socket, int backlog);
-extern ksocket_t kaccept(ksocket_t socket, int backlog);
-extern ssize_t ksend(ksocket_t socket, const void *buffer, size_t length, int flag);
+extern ksocket_t kaccept(ksocket_t socket, struct sockaddr *address, int *address_len);
+extern ssize_t ksend(ksocket_t socket, const void *buffer, size_t length, int flags);
 extern int kclose(ksocket_t socket);
 extern char *inet_ntoa(struct in_addr *in);
 
@@ -50,10 +50,47 @@ static void __exit master_exit(void);
 int master_close(struct inode *inode, struct file *filp);
 int master_open(struct inode *inode, struct file *filp);
 static long master_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param);
-static ssize_t send_msg(struct file *file, const char __user *buf, size_t count, loff_t, *data);
+static ssize_t send_msg(struct file *file, const char __user *buf, size_t count, loff_t *data);
 
 static ksocket_t sockfd_srv, sockfd_cli;
 static struct sockaddr_in addr_srv;
 static struct sockaddr_in addr_cli;
 static mm_segment_t old_fs;
 static int addr_len;
+
+static int mmap_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
+{
+  vmf->page = virt_to_page(vma->vm_private_data);
+  get_page(vmf->page);
+  return 0;
+}
+
+void mmap_open(struct vm_area_struct *vma){ /*do nothing*/ }
+
+
+void mmap_close(struct vm_area_struct *vma){ /*do nothing*/ }
+
+
+
+static const struct vm_operations_struct my_vm_ops = {
+  .fault = mmap_fault
+  .open = mmap_open,
+  .close = mmap_close,
+};
+
+static int my_mmap(struct file *file, struct vm_area_struct *vma)
+{
+  io_remap_pfn_range(vma,
+                     vma->vm_start,
+                     virt_to_phys(file->private_data) >> PAGE_SHIFT,
+                     vma->vm_end - vma->vm_start,
+                     vma->vm_page_prot);
+  vma->vm_ops = &my_vm_ops;
+  vma->vm_flags |= VM_RESERVED;
+  vma->vm_private_data = file->private_data;
+  mmap_open(vma);
+  return 0;
+}
+
+
+
