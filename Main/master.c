813031631908e6fa4524541f091998558b951e80
yingying0906 <30721578+yingying0906@users.noreply.github.com>
2019-06-10 13:33:27 +0800

    input code

--- Main/master.c
+++ Main/master.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <error.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+int main(int argc, char* argv[]){
+	/*command*/
+	char file_name[50];     /*argv[1]*/
+	char function_name[50]; /*argv[2]*/
+
+	strcpy(file, argv[1]);
+	strcpy(function, argv[2]);
+	/*-------*/
+	/*open file*/
+	int device_fd;
+	int file_fd;
+	struct timeval tv; struct timeval tz;
+
+	if((device_fd = open("master_device", O_RDWR)) < 0){
+		perror("failed to open master_device\n");
+		return 1;
+	}
+
+	gettimeofday(&tv, NULL);/*time start*/
+	
+	if((file_fd = open(file_name, O_RDWR)) < 0){
+		perror("failed to input\n");
+		return 1;
+	}
+	/*-------*/
+}
--- Main/slave.c
+++ Main/slave.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+#include <error.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+
+int main(int argc, char* argv[]){
+	/*command*/
+	char file[50];     /*argv[1]*/
+	char function[50]; /*argv[2]*/
+	char ip[50];       /*argv[3]*/
+
+	strcpy(file, argv[1]);
+	strcpy(function, argv[2]);
+	strcpy(ip, argv[3]);
+	/*-------*/
+	/*open file*/
+	int device_fd;
+	int file_fd;
+	struct timeval tv; struct timeval tz;
+
+	if((device_fd = open("slave_device", O_RDWR)) < 0){
+		perror("failed to open slave device\n");
+		return 1;
+	}
+
+	gettimeofday(&tv, NULL);/*time start*/
+	
+	if((file_fd = open(file_name, O_RDWR | O_CREAT | O_TRUNC)) < 0){
+		perror("failed to open input\n");
+		return 1;
+	}
+	/*-------*/
+}
